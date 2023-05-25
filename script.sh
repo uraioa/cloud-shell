fb4715fac7036cd0fb2b7ce82dab47f5c4596840
uraioa <123806778+uraioa@users.noreply.github.com>
2023-05-25 00:51:29 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #dd
 cd this && echo "docker run  --name this  -v ./google_compute_engine.pub:/root/.ssh/google_compute_engine.pub -v ./google_compute_engine:/root/.ssh/google_compute_engine -v ./gcloud:/root/.config/gcloud -d --rm yggdrasil536/google-cloud-shell-keep-alive" > keep.sh && cd ..
-cd that && echo "docker run  --name that  -v ./google_compute_engine.pub:/root/.ssh/google_compute_engine.pub -v ./google_compute_engine:/root/.ssh/google_compute_engine -v ./gcloud:/root/.config/gcloud -d --rm yggdrasil536/google-cloud-shell-keep-alive" > keep.sh && cd ..
+cd that && echo 'date "+%s" > time.txt && docker run  --name that  -v ./google_compute_engine.pub:/root/.ssh/google_compute_engine.pub -v ./google_compute_engine:/root/.ssh/google_compute_engine -v ./gcloud:/root/.config/gcloud -d --rm yggdrasil536/google-cloud-shell-keep-alive' > keep.sh && cd ..
 cd ../file/
 pkill -9 cf
 pkill -9 xray
